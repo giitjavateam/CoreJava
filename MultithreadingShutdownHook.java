722246d7e243ca5d249dca194d2f7d7ad65cccb0
swati2402 <sswati.2402@gmail.com>
2021-07-27 22:17:39 +0530

    MultiThreadingShutDownHook

---
+++ MultithreadingShutdownHook.java
@@ -0,0 +1,30 @@
+//class MyThread extends Thread{
+//	public void run()
+//	{
+//		System.out.println("Shutdown hook task completed");
+//	}
+//}
+public class MultithreadingShutdownHook {
+	public static void main(String args[])
+	{
+		
+		Runtime r = Runtime.getRuntime();
+		r.addShutdownHook(new Thread()
+		{
+			public void run()
+			{
+			System.out.println("Shutdown hook task completed");
+			}
+		}
+		);
+		System.out.println("Now main sleeping press ctrl+c to exit");
+		try {
+			Thread.sleep(3000);
+		}catch(Exception e)
+		{
+			
+		}
+		
+	}
+
+}
