3c8fd8eca40ca2fc03e68c8b11c285c07c7f56c9
keshav <k.jha121999@gmail.com>
2021-08-23 14:40:56 +0530

    patternProgramKeshav

---
+++ 01PatternProgram/src/Application.java
@@ -0,0 +1,25 @@
+
+public class Application {
+
+	public static void main(String[] args) {
+	    
+		for(int i=1;i<=5;i++) {
+			
+			for(int j=5;j>i;j--) {
+				System.out.print(" ");
+			}
+			
+			for(int k=i;k>=1;k--) {
+				System.out.print(k);
+			}
+			for(int k=2;k<=i;k++) {
+				System.out.print(k);
+			}
+
+			
+		
+		System.out.println();
+		
+	}
+}
+}
